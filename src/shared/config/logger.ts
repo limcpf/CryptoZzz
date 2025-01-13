08577e5997cc00681d299ed99143d9e43e06ac76
limcpf <daeseong0226@gmail.com>
2025-01-13 22:36:19 +0900

    feat: 새로운 로깅 기능 추가 (Add new logging functionality)

---
+++ src/shared/config/logger.ts
@@ -0,0 +1,33 @@
+import i18n from "../services/i18n";
+import type { MSG } from "../services/i18n/msg/msg.const";
+import webhook from "../services/webhook";
+
+const getTime = () =>
+	Intl.DateTimeFormat("ko-KR", {
+		dateStyle: "short",
+		timeStyle: "long",
+		timeZone: "Asia/Seoul",
+	}).format(new Date());
+
+const getMessage = (msg: keyof MSG, prefix?: string, suffix?: string) =>
+	`[${getTime()}] ${prefix ? `${prefix} ` : ""}${i18n.getMessage(msg)}${
+		suffix ? ` ${suffix}` : ""
+	}`;
+
+const logger = {
+	info(msg: keyof MSG, prefix?: string, suffix?: string) {
+		console.log(getMessage(msg, prefix, suffix));
+	},
+	error(msg: keyof MSG, prefix?: string, suffix?: string) {
+		const message = getMessage(msg, prefix, suffix);
+		console.error(message);
+		webhook.send(message);
+	},
+	warn(msg: keyof MSG, prefix?: string, suffix?: string) {
+		const message = getMessage(msg, prefix, suffix);
+		console.log(message);
+		webhook.send(message);
+	},
+};
+
+export default logger;

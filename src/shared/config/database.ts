c525ffeabde4af043c34e8a240410cbf12a1d81c
limcpf <daeseong0226@gmail.com>
2025-01-18 17:40:40 +0900

    feat: 자동 재연결 및 로깅 추가, notify 함수 리팩토링 (Implement database reconnection and logging; refactor notify function)

--- src/shared/config/database.ts
+++ src/shared/config/database.ts
@@ -1,7 +1,10 @@
 import { type Notification, Pool, type PoolClient } from "pg";
+import { developmentLog } from "../../services/analysis";
 import { CHANNEL, type ChannelType } from "../const/channel.const";
+import { errorHandler } from "../services/util";
+import logger from "./logger";
 
-export const createPool = () => {
+const createPool: () => Pool = () => {
 	return new Pool({
 		user: process.env.DB_USER || "coin",
 		host: process.env.DB_HOST || "localhost",
@@ -11,12 +14,59 @@ export const createPool = () => {
 	});
 };
 
+export const getConnection = async (
+	loggerPrefix: string,
+	setupCallback?: (pool: Pool, client: PoolClient) => Promise<void>,
+): Promise<[Pool, PoolClient]> => {
+	const maxReconnectAttempts = Number(process.env.MAX_RECONNECT_ATTEMPTS) || 5;
+	let reconnectAttempts = 0;
+
+	const reconnect = async (): Promise<[Pool, PoolClient]> => {
+		try {
+			if (reconnectAttempts >= maxReconnectAttempts) {
+				logger.error(
+					"DB_CONNECTION_ERROR",
+					loggerPrefix,
+					`최대 재연결 시도 횟수(${maxReconnectAttempts}회) 초과`,
+				);
+				process.exit(1);
+			}
+
+			reconnectAttempts++;
+			logger.info("RECONNECT_ATTEMPTS", loggerPrefix);
+
+			const pool = createPool();
+			const client = await pool.connect();
+
+			if (setupCallback) {
+				await setupCallback(pool, client);
+			}
+
+			return [pool, client];
+		} catch (error: unknown) {
+			errorHandler("RECONNECT_ERROR", loggerPrefix, error);
+			await new Promise((resolve) => setTimeout(resolve, 5000));
+			return reconnect();
+		}
+	};
+
+	try {
+		const pool = createPool();
+		const client = await pool.connect();
+
+		if (setupCallback) {
+			await setupCallback(pool, client);
+		}
+
+		return [pool, client];
+	} catch (error) {
+		return reconnect();
+	}
+};
+
 export const setupPubSub = async (client: PoolClient, channels: string[]) => {
 	for (const channel of channels) {
 		await client.query(`LISTEN ${channel}`);
-		console.log(
-			`[${new Date().toLocaleString()}] [LISTEN] Listening to ${channel}`,
-		);
 	}
 };
 
@@ -28,14 +78,12 @@ export const handleNotifications = (
 };
 
 export const notify = async (
-	pool: Pool,
+	client: PoolClient,
 	channel: ChannelType,
 	message = "",
 ) => {
-	if (process.env.NODE_ENV === "development") {
-		console.log(
-			`[${new Date().toLocaleString()}] [NOTIFY] ${CHANNEL[channel]} ${message}`,
-		);
-	}
-	await pool.query(`NOTIFY ${CHANNEL[channel]}, '${message}'`);
+	developmentLog(
+		`[${new Date().toLocaleString()}] [NOTIFY] ${CHANNEL[channel]} ${message}`,
+	);
+	await client.query(`NOTIFY ${CHANNEL[channel]}, '${message}'`);
 };

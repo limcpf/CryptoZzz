ae640eeb5bd0138e9bd33ec6210b0c6aef2e18aa
limcpf <daeseong0226@gmail.com>
2025-01-18 17:41:15 +0900

    feat: 데이터베이스 작업 로깅 강화를 위한 오류 처리 유틸리티 추가 (Add error handling utility for improved logging in database operations)

---
+++ src/shared/services/util.ts
@@ -0,0 +1,16 @@
+import type { PoolClient } from "pg";
+import logger from "../config/logger";
+import type { MSG } from "./i18n/msg/msg.const";
+
+export const errorHandler = (
+	client: PoolClient,
+	msg: keyof MSG,
+	loggerPrefix: string,
+	error: unknown,
+) => {
+	if (error instanceof Error) {
+		logger.error(client, msg, loggerPrefix, error.message);
+	} else {
+		logger.error(client, msg, loggerPrefix);
+	}
+};

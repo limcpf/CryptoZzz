8a9905e102a370020ea13d20921865831b545f19
limcpf <daeseong0226@gmail.com>
2025-01-25 00:34:10 +0900

    feat: Add new trading strategies to StrategyFactory (Bollinger, MACD, Stochastic) and update StrategyName enum

--- src/strategy/strategy.factory.ts
+++ src/strategy/strategy.factory.ts
@@ -1,8 +1,11 @@
 import type { PoolClient } from "pg";
 import i18n from "../shared/services/i18n";
 import type { iStrategy } from "./iStrategy";
+import { BollingerStrategy } from "./impl/bollinger.strategy";
 import { MaStrategy } from "./impl/ma.strategy";
+import { MacdStrategy } from "./impl/macd.strategy";
 import { RsiStrategy } from "./impl/rsi.strategy";
+import { StochasticStrategy } from "./impl/stochastic.strategy";
 import { VolumeStrategy } from "./impl/volume.strategy";
 
 /**
@@ -27,6 +30,12 @@ export class StrategyFactory {
 				return new MaStrategy(this.client, this.uuid, this.symbol);
 			case StrategyName.VOLUME:
 				return new VolumeStrategy(this.client, this.uuid, this.symbol);
+			case StrategyName.STOCHASTIC:
+				return new StochasticStrategy(this.client, this.uuid, this.symbol);
+			case StrategyName.MACD:
+				return new MacdStrategy(this.client, this.uuid, this.symbol);
+			case StrategyName.BOLLINGER:
+				return new BollingerStrategy(this.client, this.uuid, this.symbol);
 		}
 
 		throw new Error(
@@ -39,4 +48,7 @@ export enum StrategyName {
 	RSI = "RSI",
 	VOLUME = "VOLUME",
 	MA = "MA",
+	STOCHASTIC = "STOCHASTIC",
+	MACD = "MACD",
+	BOLLINGER = "BOLLINGER",
 }
